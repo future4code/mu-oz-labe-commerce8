f21493b5a7e22717dc6d5a2f23169b14fc38ba1c
zak almeida <phillipszak@hotmail.com>
2021-05-23 21:08:51 -0300

    Achamos

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import styled from 'styled-components';
 import meteoro from './img/meteoro.png'
 
 const BodyApp = styled.div`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
   box-sizing: border-box;
   display: grid;
   grid-template-columns: 3fr 1fr;
